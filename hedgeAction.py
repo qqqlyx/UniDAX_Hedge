8a468d5fde19a2a558b9399f6a17152a394d02e9
RobinLiu <lyx.better@gmail.com>
2018-12-11 17:34:39 +0800

    up

--- hedgeAction.py
+++ hedgeAction.py
@@ -1,8 +1,6 @@
 """
 # -*- coding: utf-8 -*-
 @author: robin.liu
-
-用来对冲成交单
 """
 
 from Huobi import HuobiServices as hbs
@@ -10,8 +8,19 @@ from UniDax import UniDaxServices as uds, Constant as cons
 import json
 import random
 
-# 获取全部成交单
+# 获取unidax仓位
+def get_unidax_position(log):
+    # 返回结果是字典
+    re_posi = {}
+    # 读取unidax持仓
+    posi = uds.account()
+    if posi['msg'] != 'suc':
+        log.error(posi)
+    else:
+        t1 = posi['data']['coin_list']  # 使用eval会报错，因次用了json方法转换str -> dict
+        for c in t1:
+            coin = c['coin']
+            vol = c['normal']
+            re_posi[coin] = vol
+    return re_posi
 
-a = uds.all_trade('ethusdt')
-
-print(a)
--- hedgeCore.py
+++ hedgeCore.py
@@ -6,49 +6,49 @@
 
 
 '''
-用来对冲成交单
+对齐持仓量的方式，来进行对冲
+UniDAX + Huobi
 '''
 
-import hedgeAction as mma
+import hedgeAction as hed
 from UniDax import UniDaxServices as uds, Constant as cons
 import threading
 import logging
+import datetime
 import json
 from pprint import pprint
 
 # 参与报价币种
-# stock_list = ['ethusdt', 'btcusdt', 'ltcusdt', 'etcusdt', 'ethbtc', 'ltcbtc']
-stock_list = ['etcusdt', 'ethusdt']
-# stock_list = ['bchusdt']
+coin_list = ['usdt','eth','etc','ltc','btc']
 
+# 用于记录基准仓位
+base_position = {}
 
 # 定期调用做市程序
 def core_timer():
     # log
     global run_count
-    log.warning('Start MM Action, ' + str(run_count))
-    # 全撤单
-    mma.mm_cancel_all(stock_list, log)
-    # 获取火币深度行情
-    huobi_quota = mma.get_huobi_depth(stock_list, log)
-    # 进行报单
-    mma.mm_trading(huobi_quota, log)
+    log.warning('Start Hedge, ' + str(run_count))
+    # 查询unidax持仓
+    posi_unidax = hed.get_unidax_position(log)
     # 定时循环
     global timer
     timer = threading.Timer(15, core_timer)
     timer.start()
     # log
-    log.warning('End MM Action, ' + str(run_count))
+    log.warning('End Hedge, ' + str(run_count))
     run_count += 1
 
 
 def set_log():
     # 创建一个logger
+    # log等级 info < warning < error
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     # 创建一个handler，用于写入日志文件
-    fh = logging.FileHandler('test.log')
-    #fh = logging.FileHandler('D://test.log')
+    date = datetime.datetime.now().strftime('%Y%m%d')
+    logname = date
+    fh = logging.FileHandler('log//' + logname)
     fh.setLevel(logging.DEBUG)
     # 再创建一个handler，用于输出到控制台
     ch = logging.StreamHandler()
@@ -61,68 +61,25 @@ def set_log():
     logger.addHandler(fh)
     logger.addHandler(ch)
     # 记录一条日志
-    logger.info('robin mm begin')
+    logger.info('robin hedge')
     return logger
 
-# log等级 info < warning < error
 
 run_count = 1
 log = set_log()
+
+
+# 获取coin基准持仓
+f = open('log//base_position.txt')
+try:
+    for l in f:
+        line = l
+        line = line.rstrip('\n') # 默认读取末尾会有换行符，这儿给删掉
+        sa = line.split(',')
+        base_position[sa[0]] = float(sa[1])
+finally:
+     f.close()
+
+# 开始定时器
 timer = threading.Timer(1, core_timer)
 timer.start()
-
-# 深度数据格式
-# 例，huobi_quota['btcusdt']['tick']['asks'][0][0] = 卖一价
-# 例，huobi_quota['btcusdt']['tick']['asks'][1][0] = 卖二价
-# 例，huobi_quota['btcusdt']['tick']['asks'][1][1] = 卖二量
-# for k in huobi_quota:
-# pprint(k)
-
-
-
-
-
-# etcusdt
-
-#print (u' 获取行情深度数据 ')
-#pprint (type(hbs.get_symbols()['data'][1]['base-currency']))
-#pprint (hbs.get_symbols()['data'])
-
-#pprint(hbs.get_depth('etcusdt','step0'))
-# def fun_timer():
-#     print('Hello Timer!')
-#     global timer
-#     timer = threading.Timer(5.5, fun_timer)
-#     timer.start()
-#
-# timer = threading.Timer(1, fun_timer)
-# timer.start()
-
-#pprint(uds.account())
-
-#pprint(uds.symbols())
-#uds.get_ticker('ethusdt')
-#uds.create_order('etcusdt','BUY','3.98','1.1')
-
-# url = "http://api.unidax.com/exchange-open-api/open/api/create_order"
-# headers = {
-#         "Accept": "application/json",
-#         'Content-Type': 'application/x-www-form-urlencoded'
-#     }
-# data={'side':'BUY',
-#       'type':'1',
-#       'volume':'1',
-#       'price':'4.34',
-#       'symbol':'etcusdt',
-#       'fee_is_user_exchange_coin':'0',
-#       'api_key':'c59256c6ae13e3d45b4c9386111a6ee4',
-#       'time':'145705845710',
-#       'sign':'a7c043600590280f38184fc50f6533dc'}
-#
-# r = requests.post(url=url, data=data,headers=headers)
-# print(r.text)
-
-#data1 = json.dumps(data)
-#requests.post(url=url, data=data,headers={'Content-Type': 'application/x-www-form-urlencoded'})
-
-#uds.new_order('btcusdt')
